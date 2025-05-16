3c41e638fc94f6248ad07e81f1257d6bf6a5dc39
jjffb4a <back4app@gmx.net>
2025-05-16 22:11:28 +0200

    🎉 Initial commit: Ember Polaris app with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{hbs,ts,js}',
+    './components/**/*.{hbs,ts,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
